71d012d105567ad933b90faab13b08afcd4aaf7d
RobbertChampagne <69464893+RobbertChampagne@users.noreply.github.com>
2021-01-01 10:02:11 +0100

    Add files via upload

---
+++ fairygamescript.js
@@ -0,0 +1,13 @@
+window.addEventListener("load", loaded);
+
+function loaded(){
+
+    //create canvas:
+    let canvas = document.getElementById("canvas");
+    let ctx = canvas.getContext('2d');
+    
+    
+    
+    
+
+}
