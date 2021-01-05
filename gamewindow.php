4ca80e6ada787925b662e2e3ec08d7de1a680acc
RobbertChampagne <69464893+RobbertChampagne@users.noreply.github.com>
2021-01-05 10:18:20 +0100

    Add files via upload

--- gamewindow.php
+++ gamewindow.php
@@ -16,14 +16,20 @@ if(!isset($_SESSION['name'])){
     <link rel="stylesheet" type="text/css" href="style.css">
     <link rel="stylesheet" type="text/css" href="fairygamewindowstyle.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script type="text/javascript" src="fairygamescript.js"></script>
 </head>
 <body>
 
     <?php include "navbar.php"; ?>
 
-    <canvas id="canvas" width="200" height="100"></canvas>
-
-    <script src="fairygamescript.js"></script>
+    <img src="images\fairygame\background.jpg" id="canvasBackgroundImg" width="200" height="100" alt="">
+    <div id="scoreLabel">Score: </div>
+    <div id="scoreCount"></div>
+    <img id="fairy" src="images\fairygame\fairy.png" alt="">
+    <a id="restartButton"><img id="restartButtonImg" src="images\fairygame\restart.png"></a>
+    
+    
+    
 
 </body>
 </html>
