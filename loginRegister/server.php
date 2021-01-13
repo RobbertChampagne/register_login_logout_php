c5af9f44327da5bd37ed59327664c027f1ecb1aa
RobbertChampagne <69464893+RobbertChampagne@users.noreply.github.com>
2021-01-13 07:43:21 +0100

    Add files via upload

--- loginRegister/server.php
+++ loginRegister/server.php
@@ -29,36 +29,43 @@ if($connection ->connect_errno) //when there is no connection
 
 //REGISTER USER
 if (isset($_POST['registerbutton'])) {
-    $name = mysqli_real_escape_string($connection, $_POST['name']); //so that escape characters are removed
-    $email = mysqli_real_escape_string($connection, $_POST['email']); 
-    $password = mysqli_real_escape_string($connection, $_POST['password']);
-    $password2 = mysqli_real_escape_string($connection, $_POST['passwordrepeat']);
-
-
+    
     //FORM VALIDATION
+    $name = filter_var($_POST['name'], FILTER_SANITIZE_SPECIAL_CHARS); //This filter is used to escape "<>& and characters with ASCII value below 32
+    $email = filter_var($_POST['email'], FILTER_SANITIZE_SPECIAL_CHARS); 
+    $email = filter_var($email, FILTER_SANITIZE_EMAIL); //remove all illegal characters from the $email variable
+    $password = filter_var($_POST['password'], FILTER_SANITIZE_SPECIAL_CHARS);
+    $password2 = filter_var($_POST['passwordrepeat'], FILTER_SANITIZE_SPECIAL_CHARS);
+
+
+    //IS EMPTY? 
     if(empty($name)) {array_push($errors, "Username is required!");}
     if(empty($email)) {array_push($errors, "Email is required!");}
     if(empty($password)) {array_push($errors, "Password is required!");}
     if($password != $password2) {array_push($errors, "Passwords need to be the same!");}
 
-    //CHECK DB FOR SAME USER WITH SAME username
-    $name_check_query = "SELECT * FROM gamers WHERE name = '$name' LIMIT 1"; //stops after finding 1
-    $result = mysqli_query($connection, $name_check_query); //use query on DB
-    $user = mysqli_fetch_assoc($result); //user result
 
-    if($user){
-        if($user['name'] === $name){array_push($errors, "Username already in use.");}
-    }
+    //CHECK DB FOR SAME USER WITH SAME username
+    $name_check_query = "SELECT name FROM gamers WHERE name = ? LIMIT 1"; //stops after finding 1
+    $result = $connection->prepare($name_check_query); //prepares query on db
+    $result->bind_param("s", $name ); //add type to var
+    $result->execute(); //uses query on DB
+    $result->bind_result($loginName); //save output query in variable
+    $result->store_result(); //save result
+
+    if($result->num_rows > 0){array_push($errors, "Username already in use.");}
 
 
     //REGISTER USER IF NO ERROR
     if(count($errors) == 0){
         $password = md5($password); //encrypt password
-        $query = "INSERT INTO gamers (name, email, password) VALUES ('$name', '$email', '$password')"; 
-        mysqli_query($connection, $query); //run query on DB
+        
+        $query = "INSERT INTO gamers (name, email, password) VALUES (?, ?, ?)";
+        $result = $connection->prepare($query); //prepares query on db
+        $result->bind_param("sss", $name, $email, $password ); //add type to var
+        $result->execute(); //uses query on DB
 
         $_SESSION['name'] = $name;
-        $_SESSION['success'] = "You are now logged in";
 
         header('location: ../homeScore/home.php');
     }
@@ -68,25 +75,35 @@ if (isset($_POST['registerbutton'])) {
 
 //LOGIN USER
 if(isset($_POST['loginbutton'])){
-    $name = mysqli_real_escape_string($connection, $_POST['name']); //so that escape characters are removed
-    $password = mysqli_real_escape_string($connection, $_POST['password']);
+    $name = filter_var($_POST['name'], FILTER_SANITIZE_SPECIAL_CHARS); //This filter is used to escape "<>& and characters with ASCII value below 32
+    $password = filter_var($_POST['password'], FILTER_SANITIZE_SPECIAL_CHARS);
 
     //FORM VALIDATION
     if(empty($name)) {array_push($errors, "Username is required!");}
     if(empty($password)) {array_push($errors, "Password is required!");}
 
     if(count($errors) == 0) {
-        $password = md5($password);
-        $query = "SELECT * FROM gamers WHERE name ='$name' AND password='$password' ";
-        $results = mysqli_query($connection, $query);
 
-        if(mysqli_num_rows($results) == 1){
-            $_SESSION['name'] = $name;
-            $_SESSION['success'] = "Logged in succesfully";
-            header("location: ../homeScore/home.php");
+        $password = md5($password);
+        $query = "SELECT name FROM gamers WHERE name = ? AND password= ? ";
+        $result = $connection->prepare($query); //prepares query
+        $result->bind_param("ss", $name, $password ); //add type to var
+        $result->execute(); //uses query on DB
+        $result->bind_result($loginName); //save output query in variable
+        $result->store_result(); //save result
+
+
+        if($result->num_rows > 0){
+            while($result->fetch()){
+                $_SESSION['name'] = $loginName;
+                header("location: ../homeScore/home.php");
+            }
         }else{
             array_push($errors, "Wrong username/password combination");
         }
+
+        $result->close();
+
     }
 }
 
